a4d0a58f8a82d661d5b499808c3c51b021a656ab
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2023-10-08 00:51:55 +0300

    Update verify.js

--- verify.js
+++ verify.js
@@ -101,27 +101,24 @@ async function fetchDataFromAPI(){
   // Send request
   const apiResponse = await cycleTLS('https://kick.com/api/v2/channels/243615/messages', {
     body: '',
-    ja3: '771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0',
-    userAgent: 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0',
+    ja3: '771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,16-17513-27-23-5-13-65037-43-0-18-65281-51-11-45-35-10,29-23-24,0',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
     headers: {
         cookie: await cookieJar.getCookieString("https://kick.com/api/v2/channels/243615/messages"),
 
     
-        "cache-control": "no-cache",
-        "upgrade-insecure-requests": "1",
-        "Accept":" application/json, text/plain, */*",
-        "sec-fetch-site":" none",
-        "sec-fetch-mode":" navigate",
-        "sec-fetch-user":" ?1",
-        "sec-fetch-dest":" document",
-        "accept-encoding":" gzip, deflate, br",
-        "accept-language":" de,en-US;q=0.7,en;q=0.3",
-        "pragma":" no-cache",
-        "te":" trailers",
-        "user-agent":" Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"
-    }   ,
-    headerOrder: ["connection", "host"]
-
+        'cache-control': 'no-cache',
+        'upgrade-insecure-requests': '1',
+        'Accept':' application/json, text/plain, */*',
+        'sec-fetch-site':' none',
+        'sec-fetch-mode':' navigate',
+        'sec-fetch-user':' ?1',
+        'sec-fetch-dest':' document',
+        'accept-encoding':' gzip, deflate, br',
+        'accept-language':' de,en-US;q=0.7,en;q=0.3',
+        'pragma':' no-cache',
+        'te':' trailers',
+    }   
   }, 'get');
 
   // Cleanly exit CycleTLS
